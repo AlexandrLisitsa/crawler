068f1c3c1feaed9833d276fbb1d8a854cfe68875
wypik <alex.lisitsa@broscorp.net>
2020-02-10 01:42:59 +0300

    fix heap

--- src/main/java/com/scrapper/storage/LocalStorageSaver.java
+++ src/main/java/com/scrapper/storage/LocalStorageSaver.java
@@ -4,7 +4,7 @@ import com.google.gson.JsonObject;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,15 +14,14 @@ public class LocalStorageSaver {
   @Value("${storagePath}")
   private String path;
 
-  public void saveContent(List<JsonObject> content) {
+  public void saveContent(JsonObject content) {
     try {
-      for (int i = 0; i < content.size(); i++) {
-        FileWriter fileWriter = new FileWriter(path + File.separator + i + ".json");
-        fileWriter.write(content.get(i).toString());
-        fileWriter.flush();
-        fileWriter.close();
-      }
-    } catch (IOException e) {
+      FileWriter fileWriter = new FileWriter(path + File.separator + new Date() + ".json");
+      fileWriter.write(content.toString());
+      fileWriter.flush();
+      fileWriter.close();
+    } catch (
+        IOException e) {
       e.printStackTrace();
     }
   }
